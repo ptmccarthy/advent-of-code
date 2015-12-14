5446fb5f03979f0fb918ac2d79057c5b4298c1fa
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-12-13 20:50:31 -0800

    Solve day 12

--- day12/12.js
+++ day12/12.js
@@ -1,7 +1,19 @@
 var input = require('./input');
+var _ = require('lodash');
 
 var sum = 0;
 
-var countObject = function(obj) {
 
+var countObject = function(obj) {
+  if (_.isArray(obj)) {
+    _.each(obj, countObject);
+  } else if (_.isObject(obj) && _.every(obj, prop => prop !== 'red')) {
+    // for part 1, remove the _.every condition looking for red
+    _.forIn(obj, countObject);
+  } else if (_.isNumber(obj)) {
+    sum += obj;
+  }
 };
+
+countObject(input);
+console.log(sum);
