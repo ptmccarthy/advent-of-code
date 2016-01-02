79d1e91d0bdb920e78f8972a1f95b3b242a5cb82
Patrick McCarthy <p.t.mccarthy@gmail.com>
2016-01-01 20:38:29 -0600

    Solve day 19 part 2

--- day19/19.js
+++ day19/19.js
@@ -9,8 +9,12 @@ fs.readFile('input.txt', 'utf8', (error, contents) => {
   // the last line of the input is the molecule
   let molecule = split[split.length - 1];
 
+  // part 1
   let results = doSubstitutions(molecule, subs);
-  console.log(results.length);
+  console.log('Part 1: ' + results.length);
+
+  // part 2
+  countTransformations(molecule);
 });
 
 const doSubstitutions = function(molecule, subs) {
@@ -36,3 +40,24 @@ const doSubstitutions = function(molecule, subs) {
 
   return _.uniq(results);
 };
+
+
+/**
+I unfortunately can not take credit for these insights about part 2, taken from
+https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju
+*/
+const countTransformations = function(molecule) {
+  // Count of all total elements in the molecule
+  let elements = molecule.match(/[A-Z]/g);
+  // "Rn" and "Ar" are boundaries of multiple possible transformations,
+  // can be thought of as "(" and ")". They are not "real" elements themselves.
+  // "Y" is the separator within an "Rn" & "Ar" boundary, similar to a ",".
+  let Rns = molecule.match(/Rn/g);
+  let Ars = molecule.match(/Ar/g);
+  let Ys = molecule.match(/Y/g);
+
+  // So, the minimum possible transformations to create the molecule starting
+  // from "e" can be described as: elements - Rns - Ars - 2*Ys - 1
+  let transforms = elements.length - Rns.length - Ars.length - (2 * Ys.length) - 1;
+  console.log('Minumum transformations required: ' + transforms);
+}
