c0c60aef9c07bc94d4f5b750ff655d8a8be8d582
Patrick McCarthy <p.t.mccarthy@gmail.com>
2016-01-02 18:27:18 -0800

    Refactor and solve part 2

--- day20/20.js
+++ day20/20.js
@@ -1,20 +1,31 @@
 'use strict';
 const input = 36000000;
 
-let calculatePresents = function(n) {
-  let presents = 0;
+let presents1 = [];
+let presents2 = [];
 
-  for (var i = 1; i <= n; i++) {
-    if (n % i === 0) {
-      presents += (10 * i);
+for (let i = 1; i < input / 10; i++) {
+  let visited = 0;
+
+  for (let j = i; j < input / 10; j = j + i) {
+    if (!presents1[j]) {
+      presents1[j] = 10;
+    }
+    presents1[j] = presents1[j] + i * 10;
+
+    if (visited < 50) {
+      if (!presents2[j]) {
+        presents2[j] = 11;
+      }
+
+      presents2[j] = presents2[j] + i * 11;
+      visited++;
     }
   }
-  return presents;
-};
-
-for (var i = 800000; i <= input; i++) {
-  if (calculatePresents(i) > input) {
-    console.log('house: ' + i);
-    break;
-  }
 }
+
+let partOne = presents1.reduce((min, current, index) => (min === 0 && current >= input) ? min = index : min, 0);
+let partTwo = presents2.reduce((min, current, index) => (min === 0 && current >= input) ? min = index : min, 0);
+
+console.log('Part 1: ' + partOne);
+console.log('Part 2: ' + partTwo);
